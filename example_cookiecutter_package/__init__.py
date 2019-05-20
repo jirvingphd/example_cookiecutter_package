f5a532f83b4115ba76774630fe58b59b58bd1150
James M. Irving <james.irving.phd@outlook.com>
2019-05-19 20:41:41 -0400

    Cookiecutter scaffolding

---
+++ example_cookiecutter_package/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Example Project Name."""
+
+__author__ = """Example Author"""
+__email__ = 'example.email@email.com'
+__version__ = '0.1.0'
---
+++ tests/test_example_cookiecutter_package.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `example_cookiecutter_package` package."""
+
+
+import unittest
+
+from example_cookiecutter_package import example_cookiecutter_package
+
+
+class TestExample_cookiecutter_package(unittest.TestCase):
+    """Tests for `example_cookiecutter_package` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
